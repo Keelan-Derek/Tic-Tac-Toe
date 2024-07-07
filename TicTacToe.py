47e6d69094ec56e9b291e1b68a3a44eee33c9645
Keelan-Derek <derekmiky@gmail.com>
2024-07-07 13:58:22 -0400

    Defining Key Functions

---
+++ TicTacToe.py
@@ -0,0 +1,31 @@
+# Tic Tac Toe game in Python
+
+def insertLetter(letter, pos):
+    pass
+
+def spaceIsFree(pos):
+    pass
+
+def printBoard(board):
+    pass
+
+def isWinner(bo, le):
+    pass
+
+def playerMove():
+    pass
+
+def compMove():
+    pass
+
+def selectRamdom(board):
+    pass
+
+def compMove():
+    pass
+
+def isBoardFull(board):
+    pass
+
+def main():
+    pass
